ef9f69e116a74c9a9148d7cba3f8ac66e040fd10
Rodrigo Brito <spensmusic@gmail.com>
2021-09-17 13:16:30 -0300

    feat: added axios to project and .env with api url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import { Main, Container } from "../styles/styled";
+const axios = require("axios");
 
 export default function Home() {
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/people/1`)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
+
   return (
     <Main>
       <span>StarWars - something</span>
--- styles/styled.ts
+++ styles/styled.ts
@@ -1,12 +1,5 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  margin: 0 15%;
-  border: 1px solid red;
-  background: gray;
-  font-size: 100px;
-`;
-
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
@@ -16,5 +9,20 @@ export const Main = styled.main`
     text-align: center;
     margin: 50px 0 25px 0;
     font-weight: 700;
+    &::after {
+      content: " -----";
+    }
+    &::before {
+      content: "----- ";
+      font-weight: 400;
+    }
   }
+`;
+
+export const Container = styled.div`
+  margin: 0 15%;
+  background: gray;
+  font-size: 100px;
+  border: none;
+  border-radius: 15px;
 `;
