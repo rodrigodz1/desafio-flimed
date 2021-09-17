e4573338facb50739baa0a84169dab5e255e55c8
Rodrigo Brito <spensmusic@gmail.com>
2021-09-17 13:39:46 -0300

    feat: returning data from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Main, Container } from "../styles/styled";
 const axios = require("axios");
 
 export default function Home() {
+  //pega todos os planetas em uma única request
+  const [planets, setPlanets] = useState([]);
+
   useEffect(() => {
     axios
-      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/people/1`)
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/planets`)
       .then(function (response) {
         // handle success
-        console.log(response);
+        setPlanets(response.data.results);
       })
       .catch(function (error) {
         // handle error
@@ -21,8 +24,20 @@ export default function Home() {
 
   return (
     <Main>
-      <span>StarWars - something</span>
-      <Container>div1</Container>
+      <span>StarWars - Planetas e Pessoas</span>
+      <Container>
+        <ul>
+          {planets ? (
+            <>
+              {planets.map((planet, i) => {
+                return <li key={i}>{planet.name}</li>;
+              })}
+            </>
+          ) : (
+            "não"
+          )}
+        </ul>
+      </Container>
     </Main>
   );
 }
--- styles/styled.ts
+++ styles/styled.ts
@@ -5,24 +5,20 @@ export const Main = styled.main`
   flex-direction: column;
   span {
     color: white;
-    font-size: 60px;
+    font-size: 50px;
     text-align: center;
     margin: 50px 0 25px 0;
     font-weight: 700;
-    &::after {
-      content: " -----";
-    }
-    &::before {
-      content: "----- ";
-      font-weight: 400;
-    }
   }
 `;
 
 export const Container = styled.div`
-  margin: 0 15%;
+  margin: 0 12%;
+  padding: 12px;
   background: gray;
-  font-size: 100px;
   border: none;
   border-radius: 15px;
+  li {
+    font-size: 50px;
+  }
 `;
